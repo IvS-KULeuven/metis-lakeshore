66c210e4e1ecb99a512d7e5018eef2ed75f6da84
Wannes Van Puyvelde <wannesvp@gmail.com>
2024-02-28 14:35:56 +0100

    Begin threading works but gives errros when changing stylesheets

--- serialcom/connect.py
+++ serialcom/connect.py
@@ -63,45 +63,48 @@ def handle_disconnect(main_window):
     except Exception as e:
         print(f"Error: {e}")
 
-def handle_connect(self):
-        i = self.connection_ui.connection_combobox.currentIndex()
-        device = self.connection_ui.devices_list[i]
-        self.port = device.device
-        self.ser = serial.Serial(self.port, self.baudrate, timeout=self.timeout)
-        read_general_information(self)
-        read_brightness(self)
-        read_input_names(self)
-        read_address(self)
-        read_slot_count(self)
-        read_slots(self)
-        read_sensor_setup(self)
-        read_curves(self)
-        read_sensor_units(self)
-        read_temperature(self)
+def handle_connect(main_window):
+        i = main_window.connection_ui.connection_combobox.currentIndex()
+        device = main_window.connection_ui.devices_list[i]
+        main_window.port = device.device
+        main_window.ser = serial.Serial(main_window.port, main_window.baudrate, timeout=main_window.timeout)
+        read_general_information(main_window)
+        read_brightness(main_window)
+        read_input_names(main_window)
+        read_address(main_window)
+        read_slot_count(main_window)
+        read_slots(main_window)
+        read_sensor_setup(main_window)
+        read_curves(main_window)
+        read_sensor_units(main_window)
+        read_temperature(main_window)
 
-        # Connect signals
-        self.general_ui.module_name_label.editingFinished.connect(lambda: handle_module_name_change(self))
-        self.profibus_ui.address_line_edit.editingFinished.connect(lambda: handle_address_change(self))
-        self.general_ui.brightness_combobox.currentIndexChanged.connect(lambda: handle_brightness_change(self))
-        self.profibus_ui.slot_combobox.currentIndexChanged.connect(lambda: handle_slot_count_change(self))
-        self.general_ui.restore_button.clicked.connect(lambda: handle_restore_factory_settings(self))
-        # Connect signals for comboboxes and others
-        for i in range(8):
-            profibus_connect_combobox(self.profibus_ui.channel_comboboxes[i],self, i)
-            profibus_connect_combobox(self.profibus_ui.units_comboboxes[i],self, i)
-            sensor_connect_type_combobox(self.sensor_ui.type_comboboxes[i], self, i)
-            sensor_connect_power_combobox(self.sensor_ui.power_comboboxes[i], self, i)
-            sensor_connect_name_edit(self.sensor_ui.name_line_edits[i], self, i)
-            sensor_connect_name_edit(self.curve_ui.name_labels[i], self, i)
-            sensor_connect_combobox(self.sensor_ui.current_reversal_comboboxes[i], self, i)
-            sensor_connect_combobox(self.sensor_ui.autorange_comboboxes[i], self, i)
-            sensor_connect_combobox(self.sensor_ui.range_comboboxes[i], self, i)
-            sensor_connect_combobox(self.sensor_ui.display_units_comboboxes[i], self, i)
-            curve_connect_delete_button(self.curve_ui.delete_buttons[i], self, i)
-            curve_connect_curve_combobox(self.curve_ui.curve_comboboxes[i], self, i)
-        
-        # start_timers()
-        self.temp_timer.start()
-        self.sensor_timer.start()
+def connect_signals(main_window):
+    # Connect signals
+    main_window.general_ui.module_name_label.editingFinished.connect(lambda: handle_module_name_change(main_window))
+    main_window.profibus_ui.address_line_edit.editingFinished.connect(lambda: handle_address_change(main_window))
+    main_window.general_ui.brightness_combobox.currentIndexChanged.connect(lambda: handle_brightness_change(main_window))
+    main_window.profibus_ui.slot_combobox.currentIndexChanged.connect(lambda: handle_slot_count_change(main_window))
+    main_window.general_ui.restore_button.clicked.connect(lambda: handle_restore_factory_settings(main_window))
+    
+    # Connect signals for comboboxes and others
+    for i in range(8):
+        profibus_connect_combobox(main_window.profibus_ui.channel_comboboxes[i],main_window, i)
+        profibus_connect_combobox(main_window.profibus_ui.units_comboboxes[i],main_window, i)
+        sensor_connect_type_combobox(main_window.sensor_ui.type_comboboxes[i], main_window, i)
+        sensor_connect_power_combobox(main_window.sensor_ui.power_comboboxes[i], main_window, i)
+        sensor_connect_name_edit(main_window.sensor_ui.name_line_edits[i], main_window, i)
+        sensor_connect_name_edit(main_window.curve_ui.name_labels[i], main_window, i)
+        sensor_connect_combobox(main_window.sensor_ui.current_reversal_comboboxes[i], main_window, i)
+        sensor_connect_combobox(main_window.sensor_ui.autorange_comboboxes[i], main_window, i)
+        sensor_connect_combobox(main_window.sensor_ui.range_comboboxes[i], main_window, i)
+        sensor_connect_combobox(main_window.sensor_ui.display_units_comboboxes[i], main_window, i)
+        curve_connect_delete_button(main_window.curve_ui.delete_buttons[i], main_window, i)
+        curve_connect_curve_combobox(main_window.curve_ui.curve_comboboxes[i], main_window, i)
+   
 
-        self.connection_ui.status_label.setText("<b>Status: </b>        Connected")
+    # start_timers()
+    main_window.temp_timer.start()
+    main_window.sensor_timer.start()
+
+    main_window.connection_ui.status_label.setText("<b>Status: </b>        Connected")
