89649692410247a685d8543c4821b004914aa3f7
Wannes Van Puyvelde <wannesvp@gmail.com>
2024-02-28 12:23:37 +0100

    Refactoring: Removed unncessary imports

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from PySide6.QtWidgets import QApplication, QWidget, QHBoxLayout
 from PySide6.QtCore import QTimer
-import serial
-import serial.tools.list_ports
 from ui.left_ui import LeftUI
 from ui.temperature_ui import TemperatureUI
-from serialcom.general import read_general_information, read_brightness, handle_module_name_change, handle_brightness_change, handle_restore_factory_settings
-from serialcom.profibus import read_address, read_slot_count, read_slots, handle_address_change, handle_slot_count_change, profibus_connect_combobox
-from serialcom.sensor import read_input_names, read_sensor_setup, sensor_connect_type_combobox, sensor_connect_name_edit, sensor_connect_power_combobox, sensor_connect_combobox
-from serialcom.curve import read_curves, curve_connect_delete_button, curve_connect_curve_combobox
 from serialcom.temperature import read_temperature, read_sensor_units
 from serialcom.connect import find_connected_devices, handle_disconnect, handle_connect
 
