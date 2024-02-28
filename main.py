858c0eb60040dfd126e2ef066cc9fc09ec07ae16
Wannes Van Puyvelde <wannesvp@gmail.com>
2024-02-28 11:42:24 +0100

    Refactoring: Moved curve functions

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from ui.temperature_ui import TemperatureUI
 from serialcom.general import read_general_information, read_brightness, handle_module_name_change, handle_brightness_change, handle_restore_factory_settings
 from serialcom.profibus import read_address, read_slot_count, read_slots, handle_address_change, handle_slot_count_change, profibus_connect_combobox
 from serialcom.sensor import read_input_names, read_sensor_setup, sensor_connect_type_combobox, sensor_connect_name_edit, sensor_connect_power_combobox, sensor_connect_combobox
-from serialcom.curve import read_curves
+from serialcom.curve import read_curves, curve_connect_delete_button, curve_connect_curve_combobox
 from serialcom.temperature import read_temperature, read_sensor_units
 
 class MainWindow(QWidget):
@@ -110,8 +110,8 @@ class MainWindow(QWidget):
             sensor_connect_combobox(self.sensor_ui.autorange_comboboxes[i], self, i)
             sensor_connect_combobox(self.sensor_ui.range_comboboxes[i], self, i)
             sensor_connect_combobox(self.sensor_ui.display_units_comboboxes[i], self, i)
-            self.curve_ui.delete_buttons[i].clicked.connect(self.handle_delete_curve)
-            self.curve_ui.curve_comboboxes[i].currentIndexChanged.connect(self.handle_curve_change)
+            curve_connect_delete_button(self.curve_ui.delete_buttons[i], self, i)
+            curve_connect_curve_combobox(self.curve_ui.curve_comboboxes[i], self, i)
         
         # start_timers()
         self.temp_timer.start()
@@ -150,134 +150,6 @@ class MainWindow(QWidget):
             self.connection_ui.status_label.setText("<b>Status: </b>        Disconnected")
         except Exception as e:
             print(f"Error: {e}")
-
-    
-    def handle_delete_curve(self):
-        sender = self.sender()
-        i = 1
-        for button in self.curve_ui.delete_buttons:
-            if (sender == button):
-                message = f"CRVDEL {i}\n"
-                self.ser.write(message.encode())
-                break
-            i+=1
-        self.curve_ui.curve_comboboxes[i-1].setCurrentIndex(-1)
-        
-
-    def handle_curve_change(self):
-        try:
-            sender = self.sender()
-            input = 0
-            i = 1
-            for combobox in self.curve_ui.curve_comboboxes:
-                if combobox == sender:
-                    input = i
-                    break
-                i +=1
-            sensor_type_box = self.sensor_ui.layout.itemAtPosition(input, 2).widget()
-            sensor_current_box = self.sensor_ui.layout.itemAtPosition(input, 3).widget()
-            sensor_autorange_box = self.sensor_ui.layout.itemAtPosition(input, 4).widget()
-            sensor_range_box = self.sensor_ui.layout.itemAtPosition(input, 5).widget()
-            sensor_unit_box = self.sensor_ui.layout.itemAtPosition(input, 6).widget()
-            file = ""
-            name = ""
-            serial = ""
-            format = 0
-            coefficient = 0
-            index = sender.currentIndex()
-            match index:
-                case 0:
-                    file = "curves/LSCI_DT600.txt"
-                    name = "LSCI_DT-600"
-                    serial = "Standard C"
-                    format = 2
-                    limit = 500
-                    coefficient = 1
-                case 1:
-                    file = "curves/LSCI_DT400.txt"
-                    name = "LSCI_DT-400"
-                    serial = "Standard C"
-                    format = 2
-                    limit = 475
-                    coefficient = 1
-                case 2:
-                    file = "curves/LSCI_PT100.txt"
-                    name = "LSCI_PT-100"
-                    serial = "STANDARD"
-                    format = 3
-                    limit = 800
-                    coefficient = 2
-                case 3:
-                    file = "curves/IEC_PT100_RTD.txt"
-                    name = "IEC_PT100_RTD"
-                    serial = "STANDARD"
-                    format = 3
-                    limit = 800
-                    coefficient = 2                
-                case 4:
-                    file = "curves/IEC_PT1000_RTD.txt"
-                    name = "IEC_PT1000_RTD"
-                    serial = "STANDARD"
-                    format = 3
-                    limit = 800
-                    coefficient = 2
-                case 5:
-                    file = "curves/SIMULATED_SENSO.txt"
-                    name = "Simulated Senso"
-                    serial = "Standard C"
-                    format = 4
-                    limit = 450
-                    coefficient = 1                    
-            if file != "":
-                message = f"CRVDEL {input}\n"
-                self.ser.write(message.encode())
-                message = f"CRVHDR {input},{name},{serial},{format},{limit},{coefficient}\n"
-                self.ser.write(message.encode())
-                with open(file, "r") as opened_file:
-                    current_index = 0
-                    for line in opened_file:
-                        current_index+=1
-                        values = line.strip().split(',')
-                        unit, temp = map(float, values)
-                        message = f"CRVPT {input},{current_index},{unit},{temp}\n"
-                        self.ser.write(message.encode())
-
-                # Change sensors
-                match index:
-                    case 0:
-                        sensor_type_box.setCurrentIndex(0)
-                        sensor_unit_box.setCurrentIndex(0)
-
-                    case 1:
-                        sensor_type_box.setCurrentIndex(0)
-                        sensor_unit_box.setCurrentIndex(0)
-
-                    case 2:
-                        sensor_type_box.setCurrentIndex(1)
-                        sensor_current_box.setCurrentIndex(1)
-                        sensor_unit_box.setCurrentIndex(0)
-
-                    case 3:
-                        sensor_type_box.setCurrentIndex(2)
-                        sensor_current_box.setCurrentIndex(0)
-                        sensor_autorange_box.setCurrentIndex(0)
-                        sensor_range_box.setCurrentIndex(2)
-                        sensor_unit_box.setCurrentIndex(2)     
-                    case 4:
-                        sensor_type_box.setCurrentIndex(2)
-                        sensor_current_box.setCurrentIndex(0)
-                        sensor_autorange_box.setCurrentIndex(0)
-                        sensor_range_box.setCurrentIndex(0)
-                        sensor_unit_box.setCurrentIndex(2)    
-                    case 5:
-                        sensor_type_box.setCurrentIndex(2)
-                        sensor_current_box.setCurrentIndex(1)
-                        sensor_autorange_box.setCurrentIndex(1)
-                        sensor_range_box.setCurrentIndex(3)
-                        sensor_unit_box.setCurrentIndex(0)
-            
-        except Exception as e:
-                print(f"Error: {e}")
 
 
 if __name__ == "__main__":
--- serialcom/curve.py
+++ serialcom/curve.py
@@ -22,3 +22,122 @@ def read_curves(main_window):
                 main_window.curve_ui.curve_comboboxes[row].setCurrentIndex(-1)
     except Exception as e:
         print(f"Error: {e}")
+
+def handle_delete_curve(main_window, i):
+    message = f"CRVDEL {i+1}\n"
+    main_window.ser.write(message.encode())
+    main_window.curve_ui.curve_comboboxes[i].setCurrentIndex(-1)
+
+def handle_curve_change(main_window, combobox, i):
+        try:
+            input = i +1
+            sensor_type_box = main_window.sensor_ui.layout.itemAtPosition(input, 2).widget()
+            sensor_current_box = main_window.sensor_ui.layout.itemAtPosition(input, 3).widget()
+            sensor_autorange_box = main_window.sensor_ui.layout.itemAtPosition(input, 4).widget()
+            sensor_range_box = main_window.sensor_ui.layout.itemAtPosition(input, 5).widget()
+            sensor_unit_box = main_window.sensor_ui.layout.itemAtPosition(input, 6).widget()
+            file = ""
+            name = ""
+            serial = ""
+            format = 0
+            coefficient = 0
+            index = combobox.currentIndex()
+            match index:
+                case 0:
+                    file = "curves/LSCI_DT600.txt"
+                    name = "LSCI_DT-600"
+                    serial = "Standard C"
+                    format = 2
+                    limit = 500
+                    coefficient = 1
+                case 1:
+                    file = "curves/LSCI_DT400.txt"
+                    name = "LSCI_DT-400"
+                    serial = "Standard C"
+                    format = 2
+                    limit = 475
+                    coefficient = 1
+                case 2:
+                    file = "curves/LSCI_PT100.txt"
+                    name = "LSCI_PT-100"
+                    serial = "STANDARD"
+                    format = 3
+                    limit = 800
+                    coefficient = 2
+                case 3:
+                    file = "curves/IEC_PT100_RTD.txt"
+                    name = "IEC_PT100_RTD"
+                    serial = "STANDARD"
+                    format = 3
+                    limit = 800
+                    coefficient = 2                
+                case 4:
+                    file = "curves/IEC_PT1000_RTD.txt"
+                    name = "IEC_PT1000_RTD"
+                    serial = "STANDARD"
+                    format = 3
+                    limit = 800
+                    coefficient = 2
+                case 5:
+                    file = "curves/SIMULATED_SENSO.txt"
+                    name = "Simulated Senso"
+                    serial = "Standard C"
+                    format = 4
+                    limit = 450
+                    coefficient = 1                    
+            if file != "":
+                message = f"CRVDEL {input}\n"
+                main_window.ser.write(message.encode())
+                message = f"CRVHDR {input},{name},{serial},{format},{limit},{coefficient}\n"
+                main_window.ser.write(message.encode())
+                with open(file, "r") as opened_file:
+                    current_index = 0
+                    for line in opened_file:
+                        current_index+=1
+                        values = line.strip().split(',')
+                        unit, temp = map(float, values)
+                        message = f"CRVPT {input},{current_index},{unit},{temp}\n"
+                        main_window.ser.write(message.encode())
+
+                # Change sensors
+                match index:
+                    case 0:
+                        sensor_type_box.setCurrentIndex(0)
+                        sensor_unit_box.setCurrentIndex(0)
+
+                    case 1:
+                        sensor_type_box.setCurrentIndex(0)
+                        sensor_unit_box.setCurrentIndex(0)
+
+                    case 2:
+                        sensor_type_box.setCurrentIndex(1)
+                        sensor_current_box.setCurrentIndex(1)
+                        sensor_unit_box.setCurrentIndex(0)
+
+                    case 3:
+                        sensor_type_box.setCurrentIndex(2)
+                        sensor_current_box.setCurrentIndex(0)
+                        sensor_autorange_box.setCurrentIndex(0)
+                        sensor_range_box.setCurrentIndex(2)
+                        sensor_unit_box.setCurrentIndex(2)     
+                    case 4:
+                        sensor_type_box.setCurrentIndex(2)
+                        sensor_current_box.setCurrentIndex(0)
+                        sensor_autorange_box.setCurrentIndex(0)
+                        sensor_range_box.setCurrentIndex(0)
+                        sensor_unit_box.setCurrentIndex(2)    
+                    case 5:
+                        sensor_type_box.setCurrentIndex(2)
+                        sensor_current_box.setCurrentIndex(1)
+                        sensor_autorange_box.setCurrentIndex(1)
+                        sensor_range_box.setCurrentIndex(3)
+                        sensor_unit_box.setCurrentIndex(0)
+            
+        except Exception as e:
+                print(f"Error: {e}")
+
+def curve_connect_delete_button(button, main_window, index):
+    button.clicked.connect(lambda: handle_delete_curve(main_window, index))
+
+def curve_connect_curve_combobox(combobox, main_window, index):
+    combobox.currentIndexChanged.connect(lambda: handle_curve_change(main_window, combobox, index))
