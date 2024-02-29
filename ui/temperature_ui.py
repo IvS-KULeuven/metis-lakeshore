6857961732689b4291a35213f88bf45eccb9cc1f
Wannes-VanPuyvelde <r0704704@ucll.be>
2024-02-29 11:14:37 +0100

    Fixed size of temperature window

--- ui/temperature_ui.py
+++ ui/temperature_ui.py
@@ -1,28 +1,40 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QSizePolicy, QTableWidget, QTableWidgetItem, QHeaderView
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QFrame, QSizePolicy, QGridLayout, QTableWidget, QTableWidgetItem, QHeaderView
+from PySide6.QtCore import Qt
 
 class TemperatureUI(QWidget):
     def __init__(self):
         super().__init__()
-        # Layout for temperature table
+
+        # Vbox for Temperature part
         self.vlayout = QVBoxLayout()
-        self.table = QTableWidget()
-        self.table.setColumnCount(5)
-        self.table.setRowCount(8)
+
+        # Label for temperature setup
+        self.temperature_label = QLabel("<b>Temperature readings</b>")
+        self.temperature_label.setStyleSheet("font-size: 14pt;")
+
+        # New QFrame for temperature section
+        self.frame = QFrame()
+        self.frame.setFrameStyle(QFrame.Panel | QFrame.Plain)
+        self.frame.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+        # Grid layout for temperature section
+        self.layout = QGridLayout()
+        self.frame.setLayout(self.layout)
+
+        # Add QTableWidget for temperature data
+        self.table = QTableWidget(8, 5)
         self.table.setHorizontalHeaderLabels(["Name", "Temperature", "Sensor units", "Excitation", "Power"])
-
-        # Populate table with sample data
-        for row in range(8):
-            self.table.setItem(row, 0, QTableWidgetItem(""))
-            self.table.setItem(row, 1, QTableWidgetItem(""))
-            self.table.setItem(row, 2, QTableWidgetItem(""))
-            self.table.setItem(row, 3, QTableWidgetItem(""))
-            self.table.setItem(row, 4, QTableWidgetItem(""))
-
-        # Set size policy to expand vertically
-        self.table.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-
-        # Set vertical header to resize to contents
         self.table.verticalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
 
-        # Add table to vlayout
-        self.vlayout.addWidget(self.table)
+        # Set fixed size of the table in pixels
+        self.table.setFixedSize(540, 240)
+
+        # Add table to layout
+        self.layout.addWidget(self.table, 1, 0, 8, 5)
+
+        # Set alignment of the general_vlayout to the left
+        self.vlayout.setAlignment(Qt.AlignTop)
+
+        # Add widgets to vlayout
+        self.vlayout.addWidget(self.temperature_label)
+        self.vlayout.addWidget(self.frame)
